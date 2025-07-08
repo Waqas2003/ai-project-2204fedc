1829bb9127d101f30d8dd74620dc8a16ea4bea76
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-08 18:52:05 +0500

    Add src\components\About.js

---
+++ src\components\About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section id="about" className="bg-white py-20">
+      <div className="container mx-auto p-4">
+        <h2 className="text-3xl font-bold">About me</h2>
+        <p className="text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet nulla auctor, vestibulum magna sed, convallis ex.</p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
