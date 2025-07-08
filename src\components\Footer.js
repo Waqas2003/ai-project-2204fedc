cdac4f56ac99befed55b2fe4c85e7e17e45edbc4
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-08 18:52:08 +0500

    Add src\components\Footer.js

---
+++ src\components\Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bg-gray-200 py-4">
+      <div className="container mx-auto p-4">
+        <p className="text-lg text-gray-600">Copyright 2023 Portfolio Website</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
